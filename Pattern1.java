24dd3d5c92713f49951a462dc318235d1c866d85
Anto Richard. S <93427534+anto-richard@users.noreply.github.com>
2023-03-14 09:17:53 +0530

    upload

---
+++ Pattern1.java
@@ -0,0 +1,17 @@
+public class Pattern1
+{
+    public static void main(String[] args) {
+        int i,j,n;
+        n=5;
+        for(i=n;i>0;i--)
+        {
+            for(j=0;j<n-i;j++)
+                System.out.print(" ");
+            for(j=1;j<=i*2-1;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
